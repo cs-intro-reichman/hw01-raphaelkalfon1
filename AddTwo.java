497131478a912cdca56d48b95e93207bc20d515c
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2023-12-25 18:11:47 +0200

    Update AddTwo.java
    fixed AddTwo

--- AddTwo.java
+++ AddTwo.java
@@ -2,6 +2,6 @@ public class AddTwo {
     public static void main(String[] args){
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
-        System.out.println("my result is "  + (x+y));
+        System.out.println(x+ " + "  + y + " = " + (x+y));
     }
 }
