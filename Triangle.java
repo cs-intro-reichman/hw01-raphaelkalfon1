94536605d0c285a9922f53b24499422ab65f27e9
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2023-12-25 19:47:24 +0200

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -3,6 +3,7 @@ public class Triangle {
      int a = Integer.parseInt(args[0]);
      int b = Integer.parseInt(args[1]);
      int c = Integer.parseInt(args[2]);
+        // I want that all the different some of two of the side is less than the third one. then I use the sign AND
      while ( a+b<c || a+c<b || b+c<a) {
         System.out.println(a + ", " + b + ", " + c + ": false");
         return;
