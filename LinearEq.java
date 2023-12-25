174b7dc489c0994b131a35dff8da14dc10ea8b3c
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2023-12-25 18:08:33 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -4,7 +4,7 @@ public class LinearEq{
       double b = Integer.parseInt(args[1]);
       double c = Integer.parseInt(args[2]);
       double x = ((c-b)/a);
-      System.out.println( a + " *x " + b + " = " + c);
+      System.out.println( a + " * x + " + b + " = " + c);
       System.out.println("X = " + x);
 
 
