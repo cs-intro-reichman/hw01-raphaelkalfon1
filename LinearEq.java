4beb1269c51bf84d9f41b8baa79194438ba9607d
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2023-12-25 19:44:57 +0200

    Update LinearEq.java

--- LinearEq.java
+++ LinearEq.java
@@ -3,6 +3,7 @@ public class LinearEq{
       double a = Integer.parseInt(args[0]);
       double b = Integer.parseInt(args[1]);
       double c = Integer.parseInt(args[2]);
+        // here I isolate x to use double
       double x = ((c-b)/a);
       System.out.println( a + " * x + " + b + " = " + c);
       System.out.println("x = " + x);
